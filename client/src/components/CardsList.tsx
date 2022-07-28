ffa5b7ae80518299f8f749c9ed9b8c76c22f82eb
IgorBayerl <bayerl.igor@gmail.com>
2022-07-27 23:17:18 -0300

    remmoving unused code

--- client/src/components/CardsList.tsx
+++ client/src/components/CardsList.tsx
@@ -138,14 +138,3 @@ export default function CardsList(props: IProps) {
     </div>
   )
 }
-
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  p: 4,
-}
